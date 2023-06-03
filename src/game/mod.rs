ed245c97c9f50175486a013a37abc366ac257e0b
Evandro Rodrigues <evandro.rodriguespj@gmail.com>
2023-06-03 20:58:49 -0300

    🔧 bug fixing and improving project structure

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -19,21 +19,20 @@ pub struct GamePlugin;
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
         app.add_state::<SimulationState>()
-            // Events
             .add_event::<GameOver>()
-            // Plugins
+            .add_system(pause_simulation.in_schedule(OnEnter(AppState::Game)))
             .add_plugin(EnemyPlugin)
             .add_plugin(PlayerPlugin)
             .add_plugin(ScorePlugin)
             .add_plugin(StarPlugin)
-            // Systems
-            .add_system(toggle_simulation.run_if(in_state(AppState::Game)));
+            .add_system(toggle_simulation.run_if(in_state(AppState::Game)))
+            .add_system(resume_simulation.in_schedule(OnExit(AppState::Game)));
     }
 }
 
 #[derive(States, Debug, Clone, Copy, Eq, PartialEq, Hash, Default)]
 pub enum SimulationState {
-    Running,
     #[default]
+    Running,
     Paused,
 }
