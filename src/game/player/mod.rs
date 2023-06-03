276c57dbcd96a2d4a077e3374bb89e8051fb6b56
Evandro Rodrigues <evandro.rodriguespj@gmail.com>
2023-06-03 15:11:42 -0300

    🎯 improving game loop and events

--- src/game/player/mod.rs
+++ src/game/player/mod.rs
@@ -5,6 +5,10 @@ mod systems;
 
 use systems::*;
 
+use crate::AppState;
+
+use super::SimulationState;
+
 #[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
 pub struct MovementSystemSet;
 
@@ -16,10 +20,24 @@ pub struct PlayerPlugin;
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.configure_set(MovementSystemSet.before(ConfinementSystemSet))
-            .add_startup_system(spawn_player)
-            .add_system(player_movement.in_set(MovementSystemSet))
-            .add_system(confine_player_movement.in_set(ConfinementSystemSet))
-            .add_system(enemy_hit_player)
-            .add_system(player_hit_star);
+            .add_system(spawn_player.in_schedule(OnEnter(AppState::Game)))
+            .add_system(
+                player_movement
+                    .in_set(MovementSystemSet)
+                    .run_if(in_state(AppState::Game))
+                    .run_if(in_state(SimulationState::Running)),
+            )
+            .add_system(
+                confine_player_movement
+                    .in_set(ConfinementSystemSet)
+                    .run_if(in_state(AppState::Game))
+                    .run_if(in_state(SimulationState::Running)),
+            )
+            .add_systems(
+                (enemy_hit_player, player_hit_star)
+                    .in_set(OnUpdate(AppState::Game))
+                    .in_set(OnUpdate(SimulationState::Running)),
+            )
+            .add_system(despawn_player.in_schedule(OnExit(AppState::Game)));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,24 @@ fn main() {
     App::new()
         // Bevy plugins
         .add_plugins(DefaultPlugins)
+        .add_state::<AppState>()
         // My plugins
         .add_plugin(MainMenuPlugin)
         .add_plugin(GamePlugin)
         // Startup systems
         .add_startup_system(spawn_camera)
         // Systems
+        .add_system(transition_to_game_state)
+        .add_system(transition_to_main_menu_state)
         .add_system(handle_game_over)
         .add_system(exit_game)
         .run()
+}
+
+#[derive(States, Debug, Clone, Copy, Eq, PartialEq, Hash, Default)]
+pub enum AppState {
+    #[default]
+    MainMenu,
+    Game,
+    GameOver,
 }
