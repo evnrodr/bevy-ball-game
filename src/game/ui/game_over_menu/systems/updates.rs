58d56708434e89161b8a083c4f48aa759ffc41b5
Evandro Rodrigues <evandro.rodriguespj@gmail.com>
2023-06-04 23:09:09 -0300

    🔧 fixing final score not updating

--- src/game/ui/game_over_menu/systems/updates.rs
+++ src/game/ui/game_over_menu/systems/updates.rs
@@ -1,15 +1,18 @@
 use bevy::prelude::*;
 
-use crate::events::GameOver;
+use crate::game::score::resources::HighScores;
 use crate::game::ui::game_over_menu::components::FinalScoreText;
 
 pub fn update_final_score_text(
-    mut game_over_event_reader: EventReader<GameOver>,
+    high_scores: ResMut<HighScores>,
     mut text_query: Query<&mut Text, With<FinalScoreText>>,
 ) {
-    for event in game_over_event_reader.iter() {
+    if high_scores.is_changed() {
         for mut text in text_query.iter_mut() {
-            text.sections[0].value = format!("Final Score: {}", event.score.to_string());
+            text.sections[0].value = format!(
+                "Final Score: {}",
+                high_scores.scores.last().unwrap().1.to_string()
+            );
         }
     }
 }
