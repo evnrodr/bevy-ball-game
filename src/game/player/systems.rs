3de25c3d339a137f2ebf9b2e593f670caa25ef5f
Evandro Rodrigues <evandro.rodriguespj@gmail.com>
2023-06-05 13:35:38 -0300

    🧪 experients with rectangular collision

--- src/game/player/systems.rs
+++ src/game/player/systems.rs
@@ -109,18 +109,41 @@ pub fn enemy_hit_player(
 ) {
     if let Ok((player_entity, player_transform)) = player_query.get_single_mut() {
         for enemy_transform in enemy_query.iter() {
-            let distance = player_transform
-                .translation
-                .distance(enemy_transform.translation);
-            let player_radius = PLAYER_SIZE / 2.0;
-            let enemy_radius = ENEMY_SIZE / 2.0;
+            // Rectangle Collision
+            let player_w = PLAYER_SIZE;
+            let player_h = PLAYER_SIZE;
+            let player_x = player_transform.translation.x;
+            let player_y = player_transform.translation.y;
 
-            if distance < player_radius + enemy_radius {
+            let enemy_w = ENEMY_SIZE;
+            let enemy_h = ENEMY_SIZE;
+            let enemy_x = enemy_transform.translation.x;
+            let enemy_y = enemy_transform.translation.y;
+
+            if player_x < enemy_x + enemy_w
+                && player_x + player_w > enemy_x
+                && player_y < enemy_y + enemy_h
+                && player_h + player_y > enemy_y
+            {
                 let sound_effect = asset_server.load("audio/explosionCrunch_000.ogg");
                 audio.play_with_settings(sound_effect, PlaybackSettings::ONCE.with_volume(0.2));
                 commands.entity(player_entity).despawn();
                 game_over_event_writer.send(GameOver { score: score.value });
             }
+
+            // Circle Collision
+            // let distance = player_transform
+            //     .translation
+            //     .distance(enemy_transform.translation);
+            // let player_radius = PLAYER_SIZE / 2.0;
+            // let enemy_radius = ENEMY_SIZE / 2.0;
+
+            // if distance < player_radius + enemy_radius {
+            // let sound_effect = asset_server.load("audio/explosionCrunch_000.ogg");
+            // audio.play_with_settings(sound_effect, PlaybackSettings::ONCE.with_volume(0.2));
+            // commands.entity(player_entity).despawn();
+            // game_over_event_writer.send(GameOver { score: score.value });
+            // }
         }
     }
 }
