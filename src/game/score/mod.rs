808fa7dbd902ae37c9ada3271d75f8c117b65859
Evandro Rodrigues <evandro.rodriguespj@gmail.com>
2023-06-05 10:19:41 -0300

    🔧 removing high score mechanic since is not being used

--- src/game/score/mod.rs
+++ src/game/score/mod.rs
@@ -3,7 +3,6 @@ use bevy::prelude::*;
 pub mod resources;
 mod systems;
 
-use resources::*;
 use systems::*;
 
 use crate::AppState;
@@ -12,9 +11,6 @@ pub struct ScorePlugin;
 
 impl Plugin for ScorePlugin {
     fn build(&self, app: &mut App) {
-        app.init_resource::<HighScores>()
-            .add_system(insert_score.in_schedule(OnEnter(AppState::Game)))
-            .add_system(update_high_scores)
-            .add_system(remove_score.in_schedule(OnExit(AppState::Game)));
+        app.add_system(insert_score.in_schedule(OnEnter(AppState::Game)));
     }
 }
--- src/ui/game_over_menu/systems/updates.rs
+++ src/ui/game_over_menu/systems/updates.rs
@@ -1,18 +1,12 @@
 use bevy::prelude::*;
 
-use crate::{game::score::resources::HighScores, ui::game_over_menu::components::FinalScoreText};
+use crate::{game::score::resources::Score, ui::game_over_menu::components::FinalScoreText};
 
 pub fn update_final_score_text(
-    high_scores: ResMut<HighScores>,
+    score: Res<Score>,
     mut text_query: Query<&mut Text, With<FinalScoreText>>,
 ) {
-    if high_scores.is_changed() {
-        println!("High Scores: {:?}", high_scores);
-        for mut text in text_query.iter_mut() {
-            text.sections[0].value = format!(
-                "Final Score: {}",
-                high_scores.scores.last().unwrap().1.to_string()
-            );
-        }
+    for mut text in text_query.iter_mut() {
+        text.sections[0].value = format!("Final Score: {}", score.value.to_string());
     }
 }
